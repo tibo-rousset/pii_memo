f5482607c528421e287a174d1cab3b15c8f5e151
Thibault Rousset <thibault.rousset@mail.mcgill.ca>
2025-11-19 17:46:23 -0500

    Compute Canada sync

--- run_train.sh
+++ run_train.sh
@@ -8,7 +8,7 @@
 #SBATCH --account=def-sreddy
 #SBATCH --gpus=1
 
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=1
 
 #SBATCH --mail-user=thibault.rousset@mail.mcgill.ca
 #SBATCH --mail-type=END
@@ -22,13 +22,13 @@ module load scipy-stack
 source $HOME/pii_memo/bin/activate
 
 echo "Job starting on $(hostname)"
+echo "Working directory: $(pwd)"
 echo "SLURM_JOB_ID=$SLURM_JOB_ID"
 echo "Using GPU(s): $SLURM_GPUS_ON_NODE"
 
 python3 -u src/simple_train.py --pile_data_path "$HOME/scratch/pii_memo/data/indicies.npy" \
-    --max_steps 1000 \
+    --config "$HOME/scratch/pii_memo/train_config.json"
     --no_download \
-    --lr 5e-3 \
-    --injection_data_path frequency_comparison_injection.json \
+    --injection_data_path debug_injection.json \
     --inject_sequence_ids pii_sequences \
-    --window_size 1024
+    --debug
